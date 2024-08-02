ebb74d80db8a8e73b11cedc753a911d0d22bc81b
gongersio <samakshagarwal27@gmail.com>
2024-08-02 12:20:51 +0100

    Added classifier.

--- src/models.py
+++ src/models.py
@@ -39,3 +39,22 @@ class Autoencoder(nn.Module):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return encoded, decoded
+    
+class Classifier(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(Classifier, self).__init__()
+
+        #First linear layer transforms the dimension of the input features to 'hidden_dim' dimensions.
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU() #Activation Function
+
+        #Second linear layer transforms the hidden layer's output to 'output_dim' dimensions (number of classes).
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.softmax(out)
+        return out
