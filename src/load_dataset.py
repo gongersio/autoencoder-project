5352e47b127f0e69617cf14b6ccc726b1a22dfd4
gongersio <samakshagarwal27@gmail.com>
2024-08-08 14:33:21 +0100

    Add classifier datasets

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -3,18 +3,17 @@ import cv2
 import subprocess
 import random
 import pickle
-
-#Import all the pytorch libraries required to create the PyTorch dataset.
-from torch.utils.data import Dataset, ConcatDataset
-from torchvision import transforms
 from PIL import Image
 
-#Define a custom dataset class that converts all the videos into a collection of representative frames. 
+#Import the necessary pytorch libraries.
+from torch.utils.data import Dataset, ConcatDataset
+from torchvision import transforms
+
 class FrameDataset(Dataset):
-    def __init__(self, dir, label):
-        '''Provide dataset directory.'''
+    def __init__(self, dir, label=0):
+        '''Converts all videos in the given directory into a representative frame dataset.'''
         self.dir = dir
-        self.label = label
+        self.label = label #Labels are used to separate the videos into distinct classes.
 
         #All frames should be transformed into pytorch tensors of a fixed size for the autoencoder.
         self.transform = transforms.Compose([
@@ -37,9 +36,9 @@ class FrameDataset(Dataset):
         return frame, self.label
 
     def extract_frames(self):
-        '''For each video, extract all frames where there is a scene change, and then select a random sample of 3 of those frames.'''
+        '''For each video, extract all frames where there is a scene change, and then select a random sample including 5 of those frames.'''
         for video in os.listdir(self.dir):
-            if video == ".frames": continue #Must exclude the 'frames' folder.
+            if video == ".frames": continue #Must exclude the temporary '.frames' folder.
             video_path = os.path.join(self.dir, video)
 
             #Create a temporary folder to store all the extracted frames.
@@ -57,35 +56,53 @@ class FrameDataset(Dataset):
 
             subprocess.run(cmd, shell=True, check=True)
 
-            #Choose a random sample of 5 from all the extracted scene change frames.  
+            #Choose a random sample of 5 out of all the extracted scene change frames.  
             extracted_frames = [os.path.join(output_path, f) for f in os.listdir(output_path)]
             frame_sample = random.sample(extracted_frames, min(len(extracted_frames), 5))
 
+            #Convert each frame to the correct format before it is added to the dataset.
             for frame_path in frame_sample:
                 frame = cv2.imread(frame_path)
-                self.frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) #Convert the frame to the correct format.
+                self.frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) 
 
-            #Clean up the temporary files.
+            #Clean up all the temporary files.
             for f in extracted_frames:
                 os.remove(f)
 
         os.rmdir(output_path)
 
 if __name__ == "__main__":
-    #The model should be trained on both types of video - tags are used to distinguish them.
-    nat_hist_train_dataset = FrameDataset("../natural_history_museum/training_videos", 0)
-    frameless_train_dataset = FrameDataset("../frameless/training_videos", 1)
-    combined_train_dataset = ConcatDataset([nat_hist_train_dataset, frameless_train_dataset])
+    #Create a frame dataset that will be used to train the autoencoder.
+    nat_hist_train = FrameDataset("../natural_history_museum/autoencoder_training")
+    frameless_train = FrameDataset("../frameless/autoencoder_training")
+    train_autoencoder = ConcatDataset([nat_hist_train, frameless_train])
 
-    nat_hist_eval_dataset = FrameDataset("../natural_history_museum/validation_videos", 0)
-    frameless_eval_dataset = FrameDataset("../frameless/validation_videos", 1)
-    combined_eval_dataset = ConcatDataset([nat_hist_eval_dataset, frameless_eval_dataset])
+    #Create a frame dataset that will be used to validate the autoencoder.
+    nat_hist_eval = FrameDataset("../natural_history_museum/autoencoder_validation")
+    frameless_eval = FrameDataset("../frameless/autoencoder_validation")
+    eval_autoencoder = ConcatDataset([nat_hist_eval, frameless_eval])
 
-    #Save the combined datasets to avoid repeating the frame extraction process.
-    with open('combined_train_dataset.pkl', 'wb') as f:
-        pickle.dump(combined_train_dataset, f)
+    #Create a frame dataset (with labels) that will be used to train the classifer.
+    nat_hist_train = FrameDataset("../natural_history_museum/classifier_training", 0)
+    frameless_train = FrameDataset("../frameless/classifier_training", 1)
+    train_classifier = ConcatDataset([nat_hist_train, frameless_train])
+
+    #Create a frame dataset (with labels) that will be used to validate the classifer.
+    nat_hist_train = FrameDataset("../natural_history_museum/classifier_validation", 0)
+    frameless_train = FrameDataset("../frameless/classifier_validation", 1)
+    eval_classifier = ConcatDataset([nat_hist_train, frameless_train])
+
+    #Save all the datasets to avoid repeating the frame extraction process.
+    with open('train_autoencoder.pkl', 'wb') as f:
+        pickle.dump(train_autoencoder, f)
     
-    with open('combined_eval_dataset.pkl', 'wb') as f:
-        pickle.dump(combined_eval_dataset, f)
+    with open('eval_autoencoder.pkl', 'wb') as f:
+        pickle.dump(eval_autoencoder, f)
+
+    with open('train_classifier.pkl', 'wb') as f:
+        pickle.dump(train_classifier, f)
+
+    with open('eval_classifier', 'wb') as f:
+        pickle.dump(eval_classifier, f)
 
     
